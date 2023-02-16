e8970aac22722853e7ac4c016c220e0b0d5e2f7e
seulgi <guseul9405@gmail.com>
2023-02-16 15:09:34 +0900

    2023.2.16 스타벅스 메인 페이지 프론트 완

--- js/main.js
+++ js/main.js
@@ -3,39 +3,125 @@
 const searchEl = document.querySelector('.search');
 const searchInputEl = searchEl.querySelector('input');
 const searchIcon = searchEl.querySelector('span');
-let isFocused = false;
 
-searchEl.addEventListener('click', function() {
-  searchInputEl.focus();
-});
+//외부 클릭 전역 변수
+let isNotSearchClick = false;
 
-searchInputEl.addEventListener('focus', function() {
-  // if(searchInputEl.classList.contains('active') === false) {
-  //   searchInputEl.placeholder = '통합검색';
-  //   searchInputEl.classList.add('active');
-  // }
-  searchInputEl.placeholder = '통합검색';
-}); //포커스 됐을 때
-
-searchInputEl.addEventListener('click', function() {
-  // if(searchInputEl.classList.contains('active') === true) {
-  //   searchInputEl.classList.remove('active');
-  //   searchInputEl.placeholder = '';
-  // } else {
-  //   searchInputEl.classList.add('active');
-  //   searchInputEl.placeholder = '통합검색';
-  // }
-  if(isFocused === true) {
-    searchInputEl.blur();
-    isFocused = false;
+document.addEventListener('click', function(e) {
+  if(e.target.classList.contains("material-symbols-outlined") === false) {
+    isNotSearchClick = true;
   } else {
-    searchInputEl.focus();
-    isFocused = true;
+    isNotSearchClick = false;
   }
 });
 
+searchInputEl.addEventListener('click', function() {
+  if(searchEl.classList.contains('active') === false) {
+    searchEl.classList.add('active');
+    searchInputEl.focus();
+    searchInputEl.setAttribute('placeholder', '통합검색');
+  } else if (isNotSearchClick === true) {
+    searchInputEl.focus();
+    searchInputEl.setAttribute('placeholder', '통합검색');
+    isNotSearchClick = false;
+  } else {
+    searchEl.classList.remove('active');
+  }
+});
+
+// searchEl.addEventListener('click', function() {
+//   searchInputEl.focus();
+// });
+// searchInputEl.addEventListener('focus', function() {
+//   searchInputEl.placeholder = '통합검색';
+// }); //포커스 됐을 때
 searchInputEl.addEventListener('blur', function() {
   searchInputEl.placeholder = '';
-  // searchInputEl.classList.remove('active');
-  // searchInputEl.blur();
 }); //포커스 없어질 때
+
+// Swiper
+// Swiper Notice
+const swiperNotice = new Swiper('section.notice .notice-line .inner .inner__left .swiper', {
+  direction : 'vertical',
+  loop : true,
+  autoplay : true,
+});
+
+// Swiper Promotion
+const swiperPromo = new Swiper('.notice .promotion .swiper', {
+  direction : 'horizontal',
+  slidesPerView : 3,
+  spaceBetween : 10,
+  centeredSlides : true,
+  loop : true,
+  autoplay : {
+    delay: 4000,
+    disableOnInteraction : false,
+  },
+  pagination : {
+    el : '.promotion .swiper-pagination',
+    clickable : true,
+  },
+  navigation : {
+    prevEl : '.promotion .swiper-button-prev',
+    nextEl : '.promotion .swiper-button-next',
+  }
+});
+
+// Autoplay 컨트롤 함수
+function controlAutoPlay() {
+  if(swiperPromo.autoplay.running === false) {
+    swiperPromo.autoplay.start();
+  } else {
+    swiperPromo.autoplay.stop();
+  }
+}
+
+// Promotion toggle 버튼
+const promotionSection = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.notice .toggle-promotion');
+promotionToggleBtn.addEventListener('click', function() {
+  if(promotionSection.classList.contains('hide') === true) {
+    promotionToggleBtn.classList.add('show');
+    promotionSection.classList.remove('hide');
+  } else {
+    promotionToggleBtn.classList.remove('show');
+    promotionSection.classList.add('hide');
+  }
+});
+
+// 애니메이션 파트
+// Visual
+window.onload = function() {
+  const visualSection = document.querySelector('.visual');
+  visualSection.classList.add('animate');
+};
+
+// Elsalvador ~ find-store의 스크롤 애니메이션
+window.addEventListener('scroll', function() {
+  console.log(window.scrollY);
+  if(window.scrollY > 693) {
+    const elsalvadorSection = document.querySelector('.elsavador');
+    elsalvadorSection.classList.add('animate');
+  }
+
+  if(window.scrollY > 1220) {
+    const ethiopiaSection = document.querySelector('.ethiopia');
+    ethiopiaSection.classList.add('animate');
+  }
+
+  if(window.scrollY > 1776) {
+    const ethiopiaSection = document.querySelector('.favorite');
+    ethiopiaSection.classList.add('animate');
+  }
+
+  if(window.scrollY > 2456) {
+    const ethiopiaSection = document.querySelector('.magazine');
+    ethiopiaSection.classList.add('animate');
+  }
+
+  if(window.scrollY > 2890) {
+    const ethiopiaSection = document.querySelector('.find-store');
+    ethiopiaSection.classList.add('animate');
+  }
+});
